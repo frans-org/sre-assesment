801ac6cb5ab5bf23ffc14ec262dcb08d9cab30d7
mshale <francis@work-test.com>
2024-02-22 11:39:33 +0000

    update var file

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "project id"
+  default = "aqueous-glyph-302220"
 }
 
 variable "region" {
   description = "region"
+  default = "us-central1"
 }
 
 variable "gke_num_nodes" {
