2789f4b24b825d634112b3a80cc53e59e267a235
mshale <francis@work-test.com>
2024-02-22 18:34:58 +0000

    amend attributes

--- infra/versions.tf
+++ infra/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/google"
       version = "4.74.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
     helm = {
        source  = "hashicorp/helm"
        version = ">= 2.1.0"
