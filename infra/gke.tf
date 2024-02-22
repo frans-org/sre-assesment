596e540eabaa74a61bcf07766fe8bc99b2638aad
mshale <francis@work-test.com>
2024-02-22 16:57:23 +0000

    feat: update dependency

--- infra/gke.tf
+++ infra/gke.tf
@@ -1,8 +1,17 @@
-data "google_client_config" "provider" {}
+# This fetches a new token, which will expire in 1 hour.
+data "google_client_config" "provider" {
+    depends_on = [ google_container_cluster.primary ]
+}
+
+# Defer reading the cluster data until the GKE cluster exists.
+data "google_container_cluster" "primary" {
+  name       = "${var.project_id}-gke"
+  depends_on = [module.gke-cluster]
+}
 
 provider "kubernetes" {
   host  = "https://${data.google_container_cluster.primary.endpoint}"
-  token                  = data.google_client_config.default.access_token
+  token = data.google_client_config.provider.access_token
     cluster_ca_certificate = base64decode(
       data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
     )
@@ -11,7 +20,7 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     host  = "https://${data.google_container_cluster.primary.endpoint}"
-    token = data.google_client_config.primary.access_token
+    token = data.google_client_config.provider.access_token
     cluster_ca_certificate = base64decode(
       data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
     )
